ad91a6a3eb1e8aeee56894e73a6e2d1200a87ef9
Jack Zemlanicky <jackzemlanicky@gmail.com>
2021-06-17 12:36:22 -0400

    Added more code and comments to better understand the dataset class

--- pytorch_dataset_template.py
+++ pytorch_dataset_template.py
@@ -1,6 +1,8 @@
 import torch
+from torch.utils import data
 from torch.utils.data import Dataset, DataLoader
 import os
+import numpy as np
 
 
 class PDFDataset(Dataset):
@@ -16,8 +18,7 @@ class PDFDataset(Dataset):
 
     def __len__(self):
         # return the number of instances of the dataset
-        lenght = 100
-        return lenght
+        return len(self.labels)
 
     def __getitem__(self, idx):
         # return X, y
@@ -30,18 +31,30 @@ class PDFDataset(Dataset):
     def _to_tensor(self, X, y):
         return torch.tensor(X, dtype=torch.float32), torch.tensor(y, dtype=torch.long)
     
-    # Get the actual dataset folders (good and bad pdfs)
+    # Get the actual dataset pdfs and load them into a dictionary(good and bad pdfs)
     def _load(self):
-        benign_files = ('/Data/CLEAN_PDF_9000_files')
-        malicious_files = ('/Data/MALWARE_PDF_PRE_04-2011_10982_files')
+        benign_files = ('Data/CLEAN_PDF_9000_files')
+        malicious_files = ('Data/MALWARE_PDF_PRE_04-2011_10982_files')
         # Dictionary to store each plot for good and bad pdfs
         data_by_type = {'benign':None,'malicious':None}
         if self.plot_type == 'byte_plot':
-            return byte_plots
-            # This is where I believe I have to use my program that converts pdf's to grayscale images using the byte plot strategy.
+            # This is where I believe I have to use my program that converts pdf's to grayscale images using the byte plot strategy and load the images into the dictionary
+            data_by_type['benign'] = 1 # put the function call here to convert
+            data_by_type['malicious'] = 1 # put the function call here to convert
+            
         elif self.plot_type == 'markov_plot':
-            return markov_plots
-            # Use Tyler's markov plot to convert each pdf from the dataset and load them into the dictionary
+            data_by_type['benign'] = 1 # put the function call here to convert
+            data_by_type['malicious'] = 1 # put the function call here to convert
+
+        # Not sure the point of this label, since we kmow from the above dictionary whether we are getting malicious or benign pdf files
+        labels = []
+        for k in data_by_type.keys():
+            n = len(data_by_type[k])
+            label = np.repeat(k,n)
+            labels.extend(label)
+        # Not exactly sure what index and  "_" are being assigned to, unelss they are both being assigned the self.findclass call
+        index, _ = self._find_class_(labels,one_hot=False)
+        self.labels = index
 
 
 def testcase_test_pdfdataset():
